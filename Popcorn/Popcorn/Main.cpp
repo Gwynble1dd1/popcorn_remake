d43979abf8dc824c21e9b18ee49e7469b4c42d75
Gwynble1dd <grivia456@gmail.com>
2023-07-07 21:00:52 +0300

    Proto_v3

--- Popcorn/Popcorn/Main.cpp
+++ Popcorn/Popcorn/Main.cpp
@@ -298,4 +298,3 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     return (INT_PTR)FALSE;
 }
 //-----------------------------------------------------------------------------------------------------------------------------------------
-//ПРОВЕРКА
