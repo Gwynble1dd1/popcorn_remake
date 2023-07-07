0728e6a1dc1623d2a0f105de1fdd4ebd007c3f58
Gwynble1dd <grivia456@gmail.com>
2023-07-07 22:51:08 +0300

    Proto_v3

--- Popcorn/Popcorn/Main.cpp
+++ Popcorn/Popcorn/Main.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "framework.h"
-#include "main.h"
+#include "Main.h"
 
 #define MAX_LOADSTRING 100
 
@@ -10,45 +10,6 @@
 HINSTANCE hInst;                                // текущий экземпляр
 WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
 WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
-
-enum EBrick_Type //создание коллекции начиная с нуля
-{
-   EBT_None,  //0
-   EBT_Red,   //1
-   RBT_Blue   //2
-};
-//Для создания кисти и ручки
-HPEN Brick_Red_Pen, Brick_Blue_Pen;
-HBRUSH Brick_Red_Brush, Brick_Blue_Brush;
-
-
-// Мои глобальные переменные
-const int Global_scale = 3;
-const int Brick_Width = 15;
-const int Brick_Height = 7;
-const int Cell_Width = 16;
-const int Cell_Height = 8;
-const int Level_X_Offest = 8;
-const int Level_Y_Offest = 6;
-
-//Объявление массива уровня
-char Level_01[14][12] = 
-{
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // пустой ряд
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, //фиолетовые кирпичи
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, // красные кирпичи
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-};
 
 
 // Отправить объявления функций, включенных в этот модуль кода:
@@ -137,15 +98,7 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //        создается и выводится главное окно программы.
 //
 //-----------------------------------------------------------------------------------------------------------------------------
-//Настройка игры т.е. инициализация
-void Init()
-{
-    Brick_Red_Pen = CreatePen(PS_SOLID, 0, RGB(255, 85, 85));
-    Brick_Red_Brush = CreateSolidBrush(RGB(255, 85, 85));
 
-    Brick_Blue_Pen = CreatePen(PS_SOLID, 0, RGB(85, 255, 255));
-    Brick_Blue_Brush = CreateSolidBrush(RGB(85, 255, 255));
-}
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    Init();
@@ -154,8 +107,8 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    RECT window_rect;
    window_rect.left = 0;
    window_rect.top = 0;
-   window_rect.right = 320 * Global_scale;
-   window_rect.bottom = 200 * Global_scale;
+   window_rect.right = 320 * 3;
+   window_rect.bottom = 200 * 3;
 
    AdjustWindowRect(&window_rect, WS_OVERLAPPEDWINDOW, TRUE);
 
@@ -171,54 +124,6 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 
    return TRUE;
 }
-//-----------------------------------------------------------------------------------------------------------------------------------------
-//Рисуем кирпичики
-void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type)
-{
-    HBRUSH brush ;
-    HPEN pen;
-
-
-	switch (brick_type)
-    {
-    case EBT_None:
-        return;
-    case EBT_Red:
-        pen = Brick_Red_Pen;
-        brush = Brick_Red_Brush;
-        break;
-    case RBT_Blue:
-        pen = Brick_Blue_Pen;
-        brush = Brick_Blue_Brush;
-        break;
-    default:
-        return;
-    }
-
-    SelectObject(hdc, brush);
-    SelectObject(hdc, pen);
-    RoundRect(hdc, x * Global_scale, y * Global_scale, (x + Brick_Width) * Global_scale, (y + Brick_Height) * Global_scale,2 * Global_scale, 2 * Global_scale);
-}
-//-----------------------------------------------------------------------------------------------------------------------------------------
-//Отрисовка всех кирпичей на уровне
-void Draw_Level(HDC hdc)
-{
-    int i, j;
-
-    for (i = 0; i < 14; i++)
-        for (j = 0; j < 12; j++)
-            Draw_Brick(hdc, Level_X_Offest + j * Cell_Width, Level_Y_Offest + i * Cell_Height, (EBrick_Type)Level_01[i][j]);
-}
-
-//-----------------------------------------------------------------------------------------------------------------------------------------
-//Отрисовка экрана игры
-void Draw_Frame(HDC hdc)
-{
-    Draw_Level(hdc);
-
-
-}
-
 //-----------------------------------------------------------------------------------------------------------------------------------------
 //
 //  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
