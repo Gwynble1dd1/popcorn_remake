8ed409017b73259aa394b10611318e8f60e960d3
Gwynble1dd <grivia456@gmail.com>
2023-08-13 09:18:28 +0300

    TODO: Learn about classes

--- Popcorn/Popcorn/Engine.h
+++ Popcorn/Popcorn/Engine.h
@@ -50,21 +50,16 @@ private:
 
     RECT Ball_Rect, Prev_Ball_Rect;
 };
+
+
 //-----------------------------------------------------------------------------------------------------------------------------------------
 class ALevel
 {
 public:
     void Init();
     void Check_Level_Brick_Hit(int &next_y_pos, double &ball_direction);
-    void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type);
-    void Set_Brick_Letter_Colors(bool is_switch_color, HPEN &front_pen, HPEN &back_pen, HBRUSH &front_brush, HBRUSH &back_brush);
-    void Draw_Brick_Letter(HDC hdc, int x, int y, EBrick_Type brick_type, ELetter_Type letter_type, int rotation_step);
     void Draw_Level(HDC hdc, RECT &paint_area);
 
-    HPEN Brick_Red_Pen, Brick_Blue_Pen, Letter_Pen;
-    HBRUSH Brick_Red_Brush, Brick_Blue_Brush;
-
-    RECT Level_Rect;
 
     static const int Level_Widtht = 12;    // Ширина уровня в ячейках (кирпичах)
     static const int Level_Height = 14;    // Высота уровня в ячейках (кирпичах)
@@ -72,9 +67,23 @@ public:
     static const int Level_Y_Offest = 6;
     static const int Cell_Width = 16;
     static const int Cell_Height = 8;
+
+private:
+    void Draw_Brick(HDC hdc, int x, int y, EBrick_Type brick_type);
+    void Set_Brick_Letter_Colors(bool is_switch_color, HPEN &front_pen, HPEN &back_pen, HBRUSH &front_brush, HBRUSH &back_brush);
+    void Draw_Brick_Letter(HDC hdc, int x, int y, EBrick_Type brick_type, ELetter_Type letter_type, int rotation_step);
+
+    HPEN Brick_Red_Pen, Brick_Blue_Pen, Letter_Pen;
+    HBRUSH Brick_Red_Brush, Brick_Blue_Brush;
+
+    RECT Level_Rect;
+
+
     static const int Brick_Width = 15;
     static const int Brick_Height = 7;
 };
+
+
 //-----------------------------------------------------------------------------------------------------------------------------------------
 class AsEngine
 {
@@ -134,4 +143,6 @@ private:
     static const int Platform_Height = 7;
 
 };
+
+
 //-----------------------------------------------------------------------------------------------------------------------------------------
